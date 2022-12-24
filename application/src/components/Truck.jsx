74c0f61e58946b9fd652b8a972486611a027c04d
RahimliZakir <zakirer@code.edu.az>
2022-12-24 16:44:11 +0400

    Truck Data Fetched

--- application/src/components/Truck.jsx
+++ application/src/components/Truck.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Card, Button, Col, Row, Container } from "react-bootstrap";
 
 import { getAllTrucks } from "../store/trucks";
 
@@ -12,11 +13,31 @@ const Truck = () => {
     // eslint-disable-next-line
   }, []);
 
-  const trucks = useSelector((state) => state.trucks);
+  const { error, list, loading } = useSelector((state) => state.trucks);
 
-  console.log(trucks);
-
-  return <div>Truck</div>;
+  return (
+    <section id="truck-section">
+      <Container>
+        <Row>
+          <h2 className="text-center">Trucks</h2>
+          {list?.map((item) => {
+            return (
+              <Col md={4} key={item.id} className="trucks-cards mt-3">
+                <Card>
+                  <Card.Img variant="top" src={item.imagePath} />
+                  <Card.Body>
+                    <Card.Title>Company: {item.company}</Card.Title>
+                    <Card.Text>Model: {item.model}</Card.Text>
+                    <Button variant="primary">Details</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>{" "}
+    </section>
+  );
 };
 
 export default Truck;
