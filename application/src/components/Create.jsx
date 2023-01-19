e7bbb149e25c22a66d5e160db2d341e6c9c64f7d
RahimliZakir <zakirer@code.edu.az>
2023-01-19 18:04:11 +0400

    Import Script Hook...(to do)

--- application/src/components/Create.jsx
+++ application/src/components/Create.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Modal, Button } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
-import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import { createTruck } from "../store/trucks";
 
@@ -30,70 +29,65 @@ const Create = ({ show, handleClose }) => {
   };
 
   return (
-    <HelmetProvider>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <div>
-            <Modal.Title>Create</Modal.Title>
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <div>
+          <Modal.Title>Create</Modal.Title>
+        </div>
+      </Modal.Header>
+      <Modal.Body>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Company"
+              {...register("company", { required: true })}
+            />
+            <small className="text-danger">
+              {errors.company?.type === "required" && (
+                <p role="alert">Bu xana doldurulmalıdır!</p>
+              )}
+            </small>
           </div>
-        </Modal.Header>
-        <Modal.Body>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Company"
-                {...register("company", { required: true })}
-              />
-              <small className="text-danger">
-                {errors.company?.type === "required" && (
-                  <p role="alert">Bu xana doldurulmalıdır!</p>
-                )}
-              </small>
-            </div>
-            <div className="form-group my-2">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Model"
-                name="model"
-                {...register("model", { required: true })}
-              />
-              <small className="text-danger">
-                {errors.model?.type === "required" && (
-                  <p role="alert">Bu xana doldurulmalıdır!</p>
-                )}
-              </small>
-            </div>
-            <div>
-              <label className="control-label">Şəkil</label>
-              <label className="image-input" htmlFor="File">
-                <input type="hidden" name="FileTemp" value="" />
-                <span>&times;</span>
-              </label>
-              <input
-                {...register("file")}
-                id="File"
-                type="file"
-                accept="image/x-png,image/gif,image/jpeg"
-              />
-            </div>
-            <Button type="submit" variant="success">
-              Add
-            </Button>
-          </form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
+          <div className="form-group my-2">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Model"
+              name="model"
+              {...register("model", { required: true })}
+            />
+            <small className="text-danger">
+              {errors.model?.type === "required" && (
+                <p role="alert">Bu xana doldurulmalıdır!</p>
+              )}
+            </small>
+          </div>
+          <div>
+            <label className="control-label">Şəkil</label>
+            <label className="image-input" htmlFor="File">
+              <input type="hidden" name="FileTemp" value="" />
+              <span>&times;</span>
+            </label>
+            <input
+              {...register("file")}
+              id="File"
+              type="file"
+              accept="image/x-png,image/gif,image/jpeg"
+            />
+          </div>
+          <Button type="submit" variant="success">
+            Add
           </Button>
-        </Modal.Footer>
-        <Helmet>
-          <script src="./libs/js/image-input.js" type="text/javascript" />
-        </Helmet>
-      </Modal>
-    </HelmetProvider>
+        </form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
