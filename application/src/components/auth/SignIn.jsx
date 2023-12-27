538f55476e0c655233b4db8f9bddf7a2eb2e55b8
RahimliZakir <zakirer@code.edu.az>
2023-12-27 01:23:32 +0100

    Some Cleanups

--- application/src/components/auth/SignIn.jsx
+++ application/src/components/auth/SignIn.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useFormik } from "formik";
